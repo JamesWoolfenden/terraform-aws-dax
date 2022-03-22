585be43168d322b062bad58c1b94a3dd99e87a1a
James Woolfenden <James.Woolfenden@gmail.com>
2022-03-22 08:26:18 +0000

    docs

--- aws_dax_cluster.demo.tf
+++ aws_dax_cluster.demo.tf
@@ -1,14 +1,15 @@
 resource "aws_dax_cluster" "demo" {
-  cluster_name           = var.cluster["cluster_name"]
-  iam_role_arn           = var.iam_role_arn
-  node_type              = var.cluster["node_type"]
-  replication_factor     = var.cluster["replication_factor"]
-  availability_zones     = var.cluster["availability_zones"]
-  description            = var.cluster["description"]
-  notification_topic_arn = var.cluster["notification_topic_arn"]
-  parameter_group_name   = aws_dax_parameter_group.example.name
-  maintenance_window     = var.cluster["maintenance_window"]
-  security_group_ids     = var.cluster["security_group_ids"]
+  cluster_name                     = var.cluster["cluster_name"]
+  iam_role_arn                     = var.iam_role_arn
+  cluster_endpoint_encryption_type = "TLS"
+  node_type                        = var.cluster["node_type"]
+  replication_factor               = var.cluster["replication_factor"]
+  availability_zones               = var.cluster["availability_zones"]
+  description                      = var.cluster["description"]
+  notification_topic_arn           = var.cluster["notification_topic_arn"]
+  parameter_group_name             = aws_dax_parameter_group.example.name
+  maintenance_window               = var.cluster["maintenance_window"]
+  security_group_ids               = var.cluster["security_group_ids"]
 
   #checkov test required
   server_side_encryption {
