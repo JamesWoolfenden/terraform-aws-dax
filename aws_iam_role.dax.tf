61fe60197acd40b31c8864a38667643084b6c8d3
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 11:33:04 +0100

    chore: pin dependencies to immutable SHAs via ghat

--- aws_iam_role.dax.tf
+++ aws_iam_role.dax.tf
@@ -1,46 +1,46 @@
 resource "aws_iam_role" "dax" {
   name_prefix = "DAXServiceRoleForDynamoDBAccess"
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-       {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "dax.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "dax.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
     ]
-})
-tags = var.common_tags
+  })
+  tags = var.common_tags
 }
 
 
 resource "aws_iam_role_policy" "dax" {
-    role = aws_iam_role.dax.name
-    policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "dynamodb:DescribeTable",
-                "dynamodb:PutItem",
-                "dynamodb:GetItem",
-                "dynamodb:UpdateItem",
-                "dynamodb:DeleteItem",
-                "dynamodb:Query",
-                "dynamodb:Scan",
-                "dynamodb:BatchGetItem",
-                "dynamodb:BatchWriteItem",
-                "dynamodb:ConditionCheckItem"
-            ],
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:dynamodb:us-west-2:${data.aws_caller_identity.current.account_id}:*"
-            ]
-        }
+  role = aws_iam_role.dax.name
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "dynamodb:DescribeTable",
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:BatchGetItem",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:ConditionCheckItem"
+        ],
+        "Effect" : "Allow",
+        "Resource" : [
+          "arn:aws:dynamodb:us-west-2:${data.aws_caller_identity.current.account_id}:*"
+        ]
+      }
     ]
-})
+  })
 
 }
 
